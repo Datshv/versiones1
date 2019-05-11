3995e3a3a1e5a252ff6cf2630f414ae43ee7f58e
Datshv <38699856+Datshv@users.noreply.github.com>
2019-05-11 15:04:34 -0500

    subir prueba

--- src/test/java/test/TestDummy.java
+++ src/test/java/test/TestDummy.java
@@ -1,5 +1,14 @@
 package test;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
 public class TestDummy {
 
+	@Test
+	public void pruebaVacia() {
+		assertEquals(true, true);
+	}
+	
 }
